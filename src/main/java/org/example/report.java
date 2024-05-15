1e1488f677328de4933473f4c580812ce113a596
Xenolfair <iemabg2020@gmail.com>
2024-05-14 19:12:39 -0500

    Clases | report & user

--- src/main/java/org/example/report.java
+++ src/main/java/org/example/report.java
@@ -3,8 +3,7 @@ package org.example;
 public class report {
     /*
     Métodos: método para generar informe de ventas, método para generar informe de inventario.
-    */
-    /*
+
     public void generateSalesReport(List<Venta> ventas) {
 
     }
